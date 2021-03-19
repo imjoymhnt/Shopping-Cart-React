1ec4e4f418faf22459469da3ccb3907dec28ed7e
Mritunjoy Mahanta <mritunjoy.mahanta001@gmail.com>
2021-03-19 18:43:09 +0530

    installed packages like axios react-toastify and also create the addToCart method

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
+import { toast } from "react-toastify";
 
 function App() {
+  const [cartItem, setCartItem] = useState([]);
+
+  const addToCart = (item) => {
+    const isAlreadyAdded = cartItem.findIndex((array) => {
+      array.id === item.id;
+    });
+    if (isAlreadyAdded !== -1) {
+      toast("Already in the cart", {
+        type: "error",
+      });
+
+      setCartItem([...cartItem, item]);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
